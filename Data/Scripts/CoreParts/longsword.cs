55a2dadc630fc9269595c4155e1620004170dde8
InvalidArgument3 <51190031+InvalidArgument3@users.noreply.github.com>
2023-04-02 19:41:14 -0500

    fixed the damage resist that fell off the back of a van

--- Data/Scripts/CoreParts/longsword.cs
+++ Data/Scripts/CoreParts/longsword.cs
@@ -25,7 +25,7 @@ namespace Scripts {
                         MuzzlePartId = "None", // The subpart where your muzzle empties are located. This is often the elevation subpart. Subpart_Boomsticks must be written as Boomsticks.
                         AzimuthPartId = "None", // Your Rotating Subpart, the bit that moves sideways.
                         ElevationPartId = "None",// Your Elevating Subpart, that bit that moves up.
-                        DurabilityMod = 1.0f, // GeneralDamageMultiplier, 0.25f = 25% damage taken.
+                        DurabilityMod = 0.25f, // GeneralDamageMultiplier, 0.25f = 25% damage taken.
                         IconName = "" // Overlay for block inventory slots, like reactors, refineries, etc.
                     },
 
